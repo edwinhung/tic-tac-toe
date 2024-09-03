f74f3737bc01653b07b3f4b9fca8a2a1f550ea0a
Edwin Hung <32249535+edwinhung@users.noreply.github.com>
2024-09-02 17:15:46 -0700

    add Player component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import Player from "./components/Player";
+
 function App() {
-  return <p>Coming soon...</p>;
+  return (
+    <menu>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
+        </ol>
+      </div>
+    </menu>
+  );
 }
 
 export default App;
