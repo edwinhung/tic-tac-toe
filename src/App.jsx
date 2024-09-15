3a98503152a6ed982d21c16e43d542d2a6e4cfc1
Edwin Hung <32249535+edwinhung@users.noreply.github.com>
2024-09-15 11:54:08 -0700

    add Log

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import { useState } from "react";
+import Log from "./components/Log";
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
@@ -40,6 +41,7 @@ function App() {
         </ol>
         <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
+      <Log turns={gameTurns} />
     </menu>
   );
 }
